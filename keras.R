2abeb65fe7f17b047b76cd2871b5e7cd915c300d
bharathdasa <bharath.r25@gmail.com>
2018-02-05 17:29:08 +0530

    Update keras.R

--- keras.R
+++ keras.R
@@ -1,10 +1,19 @@
 # To setup keras with tensorflow in R 
 
 install.packages("devtools")
-devtools::install_github("rstudio/keras")
-library(keras) 
+library("devtools")
 
-# install only one of the below two lines 
+install.packages("keras")
+library("keras") 
+
+install.packages("tensorflow")
+library("tensorflow")
 
 install_tensorflow() # if needed with cpu support 
-install_tensorflow(gpu=TRUE) # if needed with gpu support and gpu is available on the machine
+install_tensorflow(gpu=TRUE) # if needed with gpu support and gpu is available on the machin
+
+#To check if the installtion is successful. Please execute the below code
+
+sess = tf$Session()
+hello <- tf$constant('Hello, TensorFlow!')
+sess$run(hello)
